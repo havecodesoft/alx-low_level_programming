bcdd4f9162c130a959b86c55e02dab9854073c61
habte <havecodesoft@gmail.com>
2023-02-23 17:37:57 -0800

    z - a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lower;
+	
+	for (lower = 'z'; lower >= 'a'; lower--)
+		putchar(lower);
+	putchar('\n');
+
+	return (0);
+}
