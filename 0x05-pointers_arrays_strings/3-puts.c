4b576a3485adcb01a5c51aff51f7d0ebeb560506
habte <havecodesoft@gmail.com>
2023-02-27 00:29:43 -0800

    puts function

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _puts - outputs a string
+ * @string: string to be outputed
+ * Return: void
+ */
+
+void _puts(char *string)
+{
+	for (; *string != '\0'; string++)
+	{
+		_putchar(*string);
+	}
+	_putchar('\n');
+}
