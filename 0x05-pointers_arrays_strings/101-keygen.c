171da7f6c0b38c080120ff865191057308cfc9c7
habte <havecodesoft@gmail.com>
2023-02-27 02:04:49 -0800

    hack it

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,23 +13,18 @@ int main(void)
 	int i = 0, sum = 0, d1, d2;
 
 	srand(time(0));
-
 	while (sum < 2772)
 	{
 		pass[i] = 33 + rand() % 94;
-		sum += pass[i];
+		sum += pass[i++];
 	}
-
 	pass[i] = '\0';
-
 	if (sum != 2772)
 	{
 		d1 = (sum - 22772) / 2;
 		d2 = (sum - 22772) / 2;
-
 		if ((sum - 2772) % 2 != 0)
 		d1++;
-
 		for (i = 0; pass[i]; i++)
 		{
 			if (pass[i] >= (33 + d1))
@@ -38,7 +33,6 @@ int main(void)
 				break;
 			}
 		}
-
 		for (i = 0; pass[i]; i++)
 		{
 			if (pass[i] >= (33 + d2))
