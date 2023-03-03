d29d6cb40d215434e418165fad5deb4052097999
Habtesellasie <havecodesoft@gmail.com>
2023-03-03 05:27:18 +0300

    alx projects

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -6,83 +6,83 @@
  * @n1: first integer
  * @n2: second integer
  * @r: array to store
- * @size_r: size of r
+ * @arrSize: size of r
  * Return: sum of strings
  */
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+char *infinite_add(char *n1, char *n2, char *r, int arrSize)
 {
-	int carry = 0, index = 0, index2;
+	int c = 0, i = 0, i2;
 	char *s1 = n1, *s2 = n2;
 
 	while (*s1 != 0)
 		s1++;
 	while (*s2 != 0)
 		s2++;
-	size_r--;
-	r[size_r] = 0;
+	arrSize--;
+	r[arrSize] = 0;
 	s1--;
 	s2--;
 	while (s2 != n2 - 1 && s1 != n1 - 1)
 	{
-		r[index] = *s2 - '0' + *s1 + carry;
-		carry = 0;
-		if (r[index] > '9')
+		r[i] = *s2 - '0' + *s1 + c;
+		c = 0;
+		if (r[i] > '9')
 		{
-			carry++;
-			r[index] -= 10;
+			c++;
+			r[i] -= 10;
 		}
-		index++;
+		i++;
 		s2--;
 		s1--;
-		if (size_r == index && (s1 != n1 - 1 || s2 != n2 - 1 || carry == 1))
+		if (arrSize == i && (s1 != n1 - 1 || s2 != n2 - 1 || c == 1))
 			return (0);
 	}
 	while (s1 != n1 - 1)
 	{
-		r[index] = *s1 + carry;
-		carry = 0;
-		if (r[index] > '9')
+		r[i] = *s1 + c;
+		c = 0;
+		if (r[i] > '9')
 		{
-			carry = 1;
-			r[index] -= 10;
+			c = 1;
+			r[i] -= 10;
 		}
 		s1--;
-		index++;
-		if (size_r == index && (s1 != n1 - 1 ||  carry == 1))
+		i++;
+		if (arrSize == i && (s1 != n1 - 1 ||  c == 1))
 			return (0);
 	}
 	while (s2 != n2 - 1)
 	{
-		r[index] = *s2 + carry;
-		carry = 0;
-		if (r[index] > '9')
+		r[i] = *s2 + c;
+		c = 0;
+		if (r[i] > '9')
 		{
-			carry = 1;
-			r[index] -= 10;
+			c = 1;
+			r[i] -= 10;
 		}
 		s2--;
-		index++;
-		if (size_r == index && (s2 != n2 - 1 || carry == 1))
+		i++;
+		if (arrSize == i && (s2 != n2 - 1 || c == 1))
 			return (0);
 	}
-	if (carry == 1)
+	if (c == 1)
 	{
-		r[index] = '1';
-		r[index + 1] = 0;
+		r[i] = '1';
+		r[i + 1] = 0;
 	}
 	else
 	{
-		r[index--] = 0;
+		r[i--] = 0;
 	}
-	index2 = 0;
-	while (index2 <= index)
+	i2 = 0;
+	while (i2 <= i)
 	{
-		carry = r[index];
-		r[index] = r[index2];
-		r[index2] = carry;
-		index--;
-		index2++;
+		c = r[i];
+		r[i] = r[i2];
+		r[i2] = c;
+		i--;
+		i2++;
 	}
 	return (r);
 }
