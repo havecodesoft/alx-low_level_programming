49fa939cc0fe3cf0fde8532bdc6fc549eaeb0ea9
Habtesellasie <havecodesoft@gmail.com>
2023-03-03 04:25:47 +0300

    leet code

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,21 +3,26 @@
 #include <stdlib.h>
 
 /**
- * puts_half - a function that prints half of a string
+ * leet - a function that encodes string to 1337
  * @string: a string
  * Return: half of input
  */
 
-void puts_half(char *string)
+char *leet(char *string)
 {
-	int a, b, i = 0;
+	char *r = string;
+	char a[] = { 'a', 'e', 'o', 't', 'l'};
+	char n[] = { 4, 3, 0, 7, 1 };
+	int i = 0;
 
-	for (a = 0; string[a] != '\0'; a++)
-		i++;
-	b = (i / 2);
-	if ((i % 2) == 1)
-		b = ((i + 1) / 2);
-	for (a = b; string[a] != '\0'; a++)
-		putchar(string[a]);
-	putchar('\n');
+	while (*string)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*string == a[i] || *string == a[i] - 32)
+			*string = n[i] + '0';
+		}
+		string++;
+	}
+	return (r);
 }
