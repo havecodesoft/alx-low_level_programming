e805cf9b3b8561e6941c4ca48ac742366fa71d07
habte <havecodesoft@gmail.com>
2023-03-06 14:35:00 -0800

    memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memset - Entery point
+ * @str: destination
+ * @byte: consstanc byte
+ * @num: bytes
+ * Return: Always 0
+ */
+
+char *_memset(char *str, char byte, unsigned int num)
+{
+	unsigned int i;
+	for (i = 0; i < num; i++)
+		str[i] = byte;
+	return (str);
+}
